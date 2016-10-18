5cb02db0a3b98abd3fae9ed936e6ae6ef9321ffb
Cøsʍic Sαѵαɢe <jross3740@gmail.com>
2016-10-18 17:27:58 -0400

    Update SandTask.php

--- src/RainbowSand/SandTask.php
+++ src/RainbowSand/SandTask.php
@@ -8,16 +8,12 @@ use pocketmine\entity\Entity;
 class SandTask extends PluginTask{
 
 	public function __construct($main) {
-
 		$this->sand = $main;
 		parent::__construct($main);
-
 	}
 
 	public function onRun($ticks) {
-
 		$sand = $this->sand->makeSand();
 		$sand->spawnToAll();
 	}
-
 }
