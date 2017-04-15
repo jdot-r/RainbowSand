3f3cb41fb9f543ad5f053f329cf7a14208f81782
Joshua <joshua67.r@gmail.com>
2017-04-15 15:32:19 -0400

    Update Main.php

--- src/RainbowSand/Main.php
+++ src/RainbowSand/Main.php
@@ -10,68 +10,98 @@ use pocketmine\utils\Config;
 use RainbowSand\RainbowSand;
 use pocketmine\block\Block;
 
-class Main extends PluginBase{
+class Main extends PluginBase {
+	
+	const FILE = [
+		"config.yml" => [
+			"customBlock" => "false",
+			"id" => 5,
+			"damage" => 0,
+			"maxY" => 1,
+			"minY" => 1,
+			"distanceX" => 1,
+			"distanceZ" => 1,
+			"x" => 1,
+			"y" => 1,
+			"z" => 1,
+			"level" => "world",
+			"time" => 1,
+			"multipliedBy" => 15
+		],
+	];
+	
+	public static $customBlock;
+	public static $maxY;
+	public static $minY;
+	public static $distanceX;
+	public static $distanceY;
+	public static $sx;
+	public static $sy;
+	public static $sz;
+	public static $level;
+	public static $time;
+	public static $multiply;
 
-	public function onEnable() {
-		@mkdir($this->getDataFolder());
-		if(!(file_exists($this->getDataFolder(). "/config.yml"))){
-			$file = new Config($this->getDataFolder()."/config.yml", Config::YAML, array(
-				"customBlock" => "false",
-				"id" => "5",
-				"damage" => "0",
-                		"maxY" => "1",
-                		"minY" => "1",
-				"distanceX" => "1",
-                		"distanceZ" => "1",
-				"x" => "1",
-				"y" => "1",
-				"z" => "1",
-				"level" => "world",
-				"time" => "1",
-				"multipliedBy" => "15"
-			));
-			$file->save();
-		}
-		$config = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
-		$maxY = $config->get("maxY");
-		$minY = $config->get("minY");
-		$distanceX = $config->get("distanceX");
-		$distanceZ = $config->get("distanceZ");
-		$sx = $config->get("x");
-		$sy = $config->get("y");
-		$sz = $config->get("z");
-		$level = $config->get("level");
-		$time = $config->get("time");
-		$multiply = $config->get("multipliedBy");
-		Entity::registerEntity(RainbowSand::class, true);
+	public function onEnable() 
+	{
+		if(!is_dir($this->getDataFolder())) mkdir($this->getDataFolder());
+		foreach(array_keys(self::FILE) as $file) self::$updateFile($file);
+		$config = yaml_parse_file($this->getDataFolder() . "config.yml");
+		self::$maxY = $config["maxY"] ?? 1;
+		self::$minY = $config["minY"] ?? 1;
+		self::$distanceX = $config["distanceX"] ?? 1;
+		self::$distanceZ = $config["distanceZ"] ?? 1;
+		self::$sx = $config["x"] ?? 1;
+		self::$sy = $config["y"] ?? 1;
+		self::$sz = $config["z"] ?? 1;
+		self::$level = $config["level"] ?? "world";
+		self::$time = $config["time"] ?? 1;
+		self::$multiply = $config["multipliedBy"] ?? 15;
+		Entity::registerEntity(RainbowSand::class);
 		$server = Server::getInstance();
 		$spawn = $this->getServer()->getDefaultLevel();
-		$server->getScheduler()->scheduleRepeatingTask(new SandTask($this), $time * $multiply);
+		$server->getScheduler()->scheduleRepeatingTask(new SandTask($this), self::$time * self::$multiply);
         }
+	
+	private static function updateFile(string $file) 
+	{
+		if(isset(self::FILE[$file])){
+			$data = [];
+			if(is_file($path = $this->getDataFolder() . $file)){
+				$data = yaml_parse_file($path);
+			}
+			foreach(self::FILE[$file] as $key => $value){
+				if(!isset($data[$key])){
+					$data[$key] = $value;
+				}
+			}
+			yaml_emit_file($path, $data);
+		}
+	}
 
-	public function makeSand() {
-		$cfg = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
+	public function makeSand() 
+	{
+		//$cfg = new Config($this->getDataFolder() . "/config.yml", Config::YAML);
 		$randX = null;
 		$randZ = null;
-		$level = $cfg->get("level");
 		$server = $this->getServer();
-	   	if($cfg->get("level") == $server->getDefaultLevel()->getSafeSpawn()) {
- 	   		$n1 = $sx + $this->set($this->distanceX);
-	   		$n2 = $sz + $this->set($this->distanceZ);
-	   		if($n1 < $sx){
-				$randX = mt_rand($n1, $sx);
+	   	if(self::$level === $server->getDefaultLevel()->getSafeSpawn()) {
+ 	   		$n1 = self::$sx + self::$distanceX);
+	   		$n2 = self::$sz + self::$distanceZ);
+	   		if($n1 < self::$sx){
+				$randX = mt_rand($n1, self::$sx);
 				       }else{
-				$randX = mt_rand($sx, $n1);
+				$randX = mt_rand(self::$sx, $n1);
 			}
 
-	   		if($n2 < $sz){
-				$randZ = mt_rand($n2, $sz);
+	   		if($n2 < self::$sz){
+				$randZ = mt_rand($n2, self::$sz);
 	   			}else{
-				$randZ = mt_rand($sz, $n2);
+				$randZ = mt_rand(self::$sz, $n2);
 			}
-			$randY = mt_rand($sy + $this->minY, $sy + $this->maxY);
-			if($cfg->get("customBlock") == "true" || "yes"){
-				$block = Entity::createEntity("Block", $level->getChunk($randX>>4, $randZ>>4), $nbt);
+			$randY = mt_rand(self::$sy + self::$minY, self::$sy + self::$maxY);
+			if(self::$customBlock === "true"){
+				$block = Entity::createEntity("Block", self::$level->getChunk($randX >> 4, $randZ >> 4), $nbt);
 				$nbt = new CompoundTag("", [
 					"Pos" => new ListTag("Pos", [
 					new DoubleTag("", $randX),
@@ -87,15 +117,15 @@ class Main extends PluginBase{
              				new FloatTag(""),
              				new FloatTag("", 90)
            			]),
-           				"TileID" => new IntTag("TileID", $cfg->get("id")),
-           				"Data" => new ByteTag("Data", $cfg->get("damage")),
-           				"Owner" => new LongTag("Owner", $this),
+           				"TileID" => new IntTag("TileID", self::$id),
+           				"Data" => new ByteTag("Data", self::$damage)
+           				//"Owner" => new LongTag("Owner", $this),
      				]);
-				//Save
+				/*//Save
 				$this->entityBlock = $block;
-				return $block;
+				return $block;*/ //?
 			}else{
-				if($cfg->get("customBlock") == "false" || "no"){
+				if(self::$customBlock === "false"){
 					$nbt = new CompoundTag("", [
 						"Pos" => new ListTag("Pos", [
 						new DoubleTag("" , $randX),
@@ -112,14 +142,15 @@ class Main extends PluginBase{
 						new FloatTag("" , 90)
 					]),
 					]);
-					$sand = Entity::createEntity("RainbowSand", $level->getChunk($randX>>4, $randZ>>4), $nbt);
+					$sand = Entity::createEntity("RainbowSand", self::$level->getChunk($randX >> 4, $randZ >> 4), $nbt);
 					return $sand;
 				}
 			}
 		}
 	}
 
-	public function set($a) {
+	public function set($a) 
+	{
 		$n = mt_rand(1,2);
 		if($n === 1){
 			return $a;
@@ -128,4 +159,3 @@ class Main extends PluginBase{
 		}
 	}
 }
-?>
